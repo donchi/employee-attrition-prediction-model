38ddc603245f8fa72be2ea2356615b4ee50f94d5
Donchichi D Critic <cdonchichi@gmail.com>
2024-01-10 23:38:45 -0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("model/employee-attrition-predict.pk1", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+
+    output = "Yes" if prediction[0] == 1 else "No"
+    return render_template('index.html', prediction_response='Is this employee likely to leave the company: {}'.format(output))
+
+
+if __name__ == '__main__':
+    app.run()
