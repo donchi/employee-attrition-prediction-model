cc8a2623bbaa9dd8d9bc8923e88906c34b066a74
Donchichi D Critic <cdonchichi@gmail.com>
2024-01-12 01:53:24 -0200

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = pickle.load(open("model/employee-attrition-predict.pk1", "rb"))
 def home():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
     features = [np.array(int_features)]
@@ -19,3 +19,5 @@ def predict():
     return render_template('index.html', prediction_response='Is this employee likely to leave the company: {}'.format(output))
 
 
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
